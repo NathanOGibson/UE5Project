3b437f040767ad946b28aa6bf07cbd180af08f01
Nate Gibson <ng7772@gmail.com>
2024-02-16 22:54:11 +1300

    Created Unreal Engine 5 Project

---
+++ README.md
@@ -0,0 +1,3 @@
+# UE5VersonControl
+
+Developed with Unreal Engine 5
---
+++ Source/UE5VersonControl/UE5VersonControlGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE5VersonControlGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5VERSONCONTROL_API AUE5VersonControlGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
